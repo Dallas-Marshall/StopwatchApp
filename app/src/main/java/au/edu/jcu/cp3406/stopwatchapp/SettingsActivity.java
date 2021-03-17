6ee3c85f6f1133d85982e83b63dbd65d12b03537
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-17 12:26:02 +1000

    Add speedEntry and done button with handler

--- app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/SettingsActivity.java
@@ -2,13 +2,29 @@ package au.edu.jcu.cp3406.stopwatchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+    public static final int SETTINGS_REQUEST = 1;
+
+    private EditText speedEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void doneClicked(View view) {
+        String text = speedEntry.getText().toString();
+        int speed = Integer.parseInt(text);
+
+        Intent intent = new Intent();
+        intent.putExtra("speed", speed);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
