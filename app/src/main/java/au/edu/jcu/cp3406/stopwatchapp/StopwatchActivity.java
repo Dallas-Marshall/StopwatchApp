2e4e432565bafd57b7485f530e8883ab7f435dcd
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-17 11:28:38 +1000

    Add start and stop methods, toggle handler and onSaveInstanceState

--- app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
@@ -1,14 +1,78 @@
 package au.edu.jcu.cp3406.stopwatchapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
+    private Stopwatch stopwatch;
+    private Handler handler;
+    private boolean isRunning;
+    private TextView stopwatchTimeDisplay;
+    private Button stopwatchToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stopwatch_activity);
+
+        stopwatchTimeDisplay = findViewById(R.id.stopwatchTimeDisplay);
+        stopwatchToggle = findViewById(R.id.stopwatchToggle);
+
+        isRunning = false;
+        if (savedInstanceState == null) { // No saved object state
+            stopwatch = new Stopwatch();
+        } else {
+            stopwatch = new Stopwatch(savedInstanceState.getString("Value"));
+            boolean running = savedInstanceState.getBoolean("running");
+            if (running) {
+                enableStopwatch();
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("Value", stopwatch.toString());
+        outState.putBoolean("running", isRunning);
+    }
+
+    private void enableStopwatch() {
+        isRunning = true;
+        handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (isRunning) {
+                    stopwatch.tick();
+                    stopwatchTimeDisplay.setText(stopwatch.toString());
+                    handler.postDelayed(this, 1000); // Wait 1000 ms before running again
+                }
+            }
+        });
+    }
+
+    private void disableStopwatch() {
+        isRunning = false;
+    }
+
+    public void buttonClicked(View view) {
+        if (!isRunning) {
+            enableStopwatch();
+        } else {
+            disableStopwatch();
+        }
+        updateStopwatchToggleText();
+    }
+
+    private void updateStopwatchToggleText() {
+        stopwatchToggle.setText(isRunning ? "Stop" : "Start");
     }
 }
